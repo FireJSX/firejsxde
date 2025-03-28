577cf0753a9053d00f3db7ab4747e04584cc47f6
Jonas <vogel53@ads.uni-passau.de>
2025-03-28 16:13:07 +0100

    photography pre

--- src/pages/Photography.tsx
+++ src/pages/Photography.tsx
@@ -9,72 +9,93 @@ const photos = [
         date: "01 Mar 2025",
         location: "Dresden, Germany",
         description: "A cloudy day next to the Theaterplatz.",
-        blurColor: "rgba(255, 99, 71, 0.1)",
+        //blurColor: "rgba(255, 99, 71, 0.1)",
     },
     {
         src: "/images/photography/photo-2.jpg",
         date: "03 Mar 2025",
         location: "Dresden, Germany",
         description: "Sunset over the Theaterplatz.",
-        blurColor: "rgba(30, 144, 255, 0.1)",
+        //blurColor: "rgba(30, 144, 255, 0.1)",
     },
     {
         src: "/images/photography/photo-3.jpg",
         date: "19 Jul 2024",
         location: "Friedrichshafen, Germany",
         description: "Sunny day at the Bodensee.",
-        blurColor: "rgba(50, 205, 50, 0.1)",
+        //blurColor: "rgba(50, 205, 50, 0.1)",
     },
     {
         src: "/images/photography/photo-4.jpg",
         date: "19 Jun 2024",
         location: "Deggendorf, Germany",
         description: "Sunset walk in Deggendorf.",
-        blurColor: "rgba(250, 0, 250, 0.1)",
+        //blurColor: "rgba(250, 0, 250, 0.1)",
     },
     {
         src: "/images/photography/photo-5.jpg",
         date: "19 Jun 2024",
         location: "Deggendorf, Germany",
         description: "Locks near the Donau in Deggendorf.",
-        blurColor: "rgba(250, 255, 50, 0.1)",
+        //blurColor: "rgba(250, 255, 50, 0.1)",
     },
     {
         src: "/images/photography/photo-6.jpg",
         date: "01 Mar 2025",
         location: "Dresden, Germany",
         description: "A cloudy day at the Zwinger in Dresden.",
-        blurColor: "rgba(0, 205, 100, 0.1)",
+        //blurColor: "rgba(0, 205, 100, 0.1)",
     },
     {
         src: "/images/photography/photo-7.jpg",
         date: "01 Mar 2025",
         location: "Dresden, Germany",
         description: "Action on the Theaterplatz.",
-        blurColor: "rgba(200, 200, 200, 0.1)",
+        //blurColor: "rgba(200, 200, 200, 0.1)",
     },
 ];
 
 const Photography: React.FC = () => {
+    const columnCount = 3; // Anzahl der Spalten, die wir haben wollen
+    const columns = Array.from({ length: columnCount }, () => []); // Spaltenarray für die Bilder
+
+    // Bilder gleichmäßig den 3 Spalten zuweisen
+    photos.forEach((photo, index) => {
+        columns[index % columnCount].push(photo);
+    });
+
+    // @ts-ignore
     return (
         <div>
             <Navbar />
+            <header className="hero">
+                    <h1 className="hero-text">Photography</h1>
+            </header>
             <main className="photography">
                 <div className="photo-gallery">
-                    {photos.map((photo, index) => (
-                        <div className="photo-column" key={index}>
-                            <div className="photo-container">
-                                <img src={photo.src} alt={photo.location} className="photo" />
-                                <div className="photo-info">
-                                    <div className="photo-location">{photo.location}</div>
-                                    <div className="photo-date">{photo.date}</div>
-                                    <div className="photo-description">{photo.description}</div>
+                    {columns.map((column, colIndex) => (
+                        <div className="photo-column" key={colIndex}>
+                            {column.map((photo, index) => (
+                                <div className="photo-container" key={index}>
+                                    <img src={photo.src} alt={photo.location} className="photo" />
+                                    <div className="photo-info">
+                                        <div className="photo-location">{photo.location}</div>
+                                        <div className="photo-date">{photo.date}</div>
+                                        <div className="photo-description">{photo.description}</div>
+                                    </div>
                                 </div>
-                            </div>
+                            ))}
                         </div>
                     ))}
                 </div>
             </main>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
             <Footer />
         </div>
     );
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    root: '.',  // Sicherstellen, dass der Root-Ordner korrekt eingestellt ist
+    root: '.',
     build: {
         rollupOptions: {
-            input: 'index.html', // Hier wird explizit die index.html als Einstiegspunkt angegeben
+            input: 'index.html',
         },
     },
 });
