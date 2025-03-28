a0ec5d66171a951274de6dc454c31980fb517c20
Jonas <vogel53@ads.uni-passau.de>
2025-03-28 17:25:34 +0100

    about pre

--- src/pages/Photography.tsx
+++ src/pages/Photography.tsx
@@ -3,61 +3,69 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import '../assets/styles/main.scss';
 
-const photos = [
+interface Photo {
+    src: string;
+    date: string;
+    location: string;
+    description: string;
+    blurColor: string;
+}
+
+const photos: Photo[] = [
     {
         src: "/images/photography/photo-1.jpg",
         date: "01 Mar 2025",
         location: "Dresden, Germany",
         description: "A cloudy day next to the Theaterplatz.",
-        //blurColor: "rgba(255, 99, 71, 0.1)",
+        blurColor: "rgba(255, 99, 71, 0.1)",
     },
     {
         src: "/images/photography/photo-2.jpg",
         date: "03 Mar 2025",
         location: "Dresden, Germany",
         description: "Sunset over the Theaterplatz.",
-        //blurColor: "rgba(30, 144, 255, 0.1)",
+        blurColor: "rgba(30, 144, 255, 0.1)",
     },
     {
         src: "/images/photography/photo-3.jpg",
         date: "19 Jul 2024",
         location: "Friedrichshafen, Germany",
         description: "Sunny day at the Bodensee.",
-        //blurColor: "rgba(50, 205, 50, 0.1)",
+        blurColor: "rgba(50, 205, 50, 0.1)",
     },
     {
         src: "/images/photography/photo-4.jpg",
         date: "19 Jun 2024",
         location: "Deggendorf, Germany",
         description: "Sunset walk in Deggendorf.",
-        //blurColor: "rgba(250, 0, 250, 0.1)",
+        blurColor: "rgba(250, 0, 250, 0.1)",
     },
     {
         src: "/images/photography/photo-5.jpg",
         date: "19 Jun 2024",
         location: "Deggendorf, Germany",
         description: "Locks near the Donau in Deggendorf.",
-        //blurColor: "rgba(250, 255, 50, 0.1)",
+        blurColor: "rgba(250, 255, 50, 0.1)",
     },
     {
         src: "/images/photography/photo-6.jpg",
         date: "01 Mar 2025",
         location: "Dresden, Germany",
         description: "A cloudy day at the Zwinger in Dresden.",
-        //blurColor: "rgba(0, 205, 100, 0.1)",
+        blurColor: "rgba(0, 205, 100, 0.1)",
     },
     {
         src: "/images/photography/photo-7.jpg",
         date: "01 Mar 2025",
         location: "Dresden, Germany",
         description: "Action on the Theaterplatz.",
-        //blurColor: "rgba(200, 200, 200, 0.1)",
+        blurColor: "rgba(200, 200, 200, 0.1)",
     },
 ];
 
 const Photography: React.FC = () => {
     const columnCount = 3; // Anzahl der Spalten, die wir haben wollen
-    const columns = Array.from({ length: columnCount }, () => []); // Spaltenarray für die Bilder
+    const columns: Photo[][] = Array.from({ length: columnCount }, () => []); // Spaltenarray für die Bilder
 
     // Bilder gleichmäßig den 3 Spalten zuweisen
     photos.forEach((photo, index) => {
