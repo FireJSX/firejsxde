92b14287024a9caf86f06309a6c6e24ba5c7c4bd
Jonas Vogel <firestroke.ghg@gmail.com>
2025-06-11 10:16:12 +0200

    Note for Download

--- src/pages/Development.tsx
+++ src/pages/Development.tsx
@@ -87,6 +87,7 @@ const Development: React.FC = () => {
                                 <li><strong>Move:</strong> Press A/D to move your dino.</li>
                                 <li><strong>Pause:</strong> Press ESC to open the pause menu.</li>
                                 <li><strong>Quit:</strong> Press F12 or close the window.</li>
+                                <li><strong>Note: If you want to download the game you will get 2 split packages. You need both to extract the main .zip file!</li>
                             </ul>
                         </div>
 
